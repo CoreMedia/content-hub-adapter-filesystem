55557d4239b1b5a00e479688336b9cc8329d69a3
Marco Trani <marco.trani@coremedia.com>
2024-09-24 10:48:59 +0200

    update all possible dependencies and lint rules

--- studio-client/.eslintrc.js
+++ studio-client/.eslintrc.js
@@ -1,96 +1,42 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2021,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "@typescript-eslint",
-    "import",
-    "jsdoc",
-    "unused-imports",
+  // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser",
+  // Recommended rules from other configs and plugins
+  extends: [
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    //"plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  "rules": {
-    "@typescript-eslint/brace-style": "error",
-    "@typescript-eslint/comma-dangle": [
-      "error",
-      "always-multiline"
-    ],
-    "@typescript-eslint/comma-spacing": "error",
-    "@typescript-eslint/object-curly-spacing": ["error", "always"],
-    "@typescript-eslint/func-call-spacing": "error",
-    "@typescript-eslint/keyword-spacing": "error",
-    "@typescript-eslint/lines-between-class-members": "error",
-    "@typescript-eslint/quotes": [
-      "error",
-      "double"
-    ],
-    "@typescript-eslint/space-before-function-paren": ["error", {
-      "anonymous": "never",
-      "named": "never",
-      "asyncArrow": "always",
-    }],
-    "@typescript-eslint/space-infix-ops": "error",
-    "@typescript-eslint/type-annotation-spacing": "error",
-    "space-before-blocks": "error",
-    "array-bracket-newline": ["error", "consistent"],
-    "arrow-parens": ["error", "as-needed"],
-    "prefer-const": "error",
-    "import/no-duplicates": "error",
-    "import/newline-after-import": [
-      "error", {"count": 1}
-    ],
-    "import/order": ["error", {
-      "alphabetize": {
-        "order": "asc"
-      },
-    }],
-    "indent": ["error", 2],
-    "jsdoc/check-alignment": [
-      "error"
-    ],
-    "key-spacing": ["error", {
-      "mode": "strict",
-    }],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "no-mixed-spaces-and-tabs": "error",
-    "no-multiple-empty-lines": ["error", {
-      "max": 1
-    }],
-    "no-multi-spaces": "error",
-    "no-trailing-spaces": "error",
-    "no-var": "error",
-    "no-whitespace-before-property": "error",
-    "object-curly-newline": ["error", {
-      "multiline": true
-    }],
-    "object-property-newline": "error",
-    "padding-line-between-statements": "off",
-    "@typescript-eslint/padding-line-between-statements": [
-      "error",
+  // TypeScript parser configuration, see https://typescript-eslint.io/architecture/parser#configuration
+  parserOptions: {
+    ecmaVersion: "latest", // default = "2018"
+  },
+  // Exclude default output folders, see https://eslint.org/docs/latest/use/configure/ignore
+  ignorePatterns: [
+    // If a glob pattern starts with /, the pattern is relative to the base directory of the config file.
+    "/build/",
+    "/dist/",
+    "/target/",
+  ],
+  // Custom CoreMedia rules for JS/TS packages
+  rules: {
+    "import/no-unresolved": "off",
+    "import/order": ["error",
       {
-        "blankLine": "always",
-        "prev": ["class", "interface", "const"],
-        "next": ["class", "interface"],
-      },
-      {
-        "blankLine": "always",
-        "prev": "*",
-        "next": "export",
+        "newlines-between": "never",
       }
     ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "space-in-parens": "error",
-    "unused-imports/no-unused-imports": "error",
-  }
+    "import/newline-after-import": ["error"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-unsafe-declaration-merging": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "prefer-rest-params": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+    "@typescript-eslint/no-empty-object-type": ["error", {
+      allowInterfaces: "always",
+    }],
+  },
 };
